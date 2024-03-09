198ac80c43dfb2a67096f97db580e314ff4e6b57
Dearborn42 <amurf26@outlook.com>
2024-03-09 01:47:33 -0700

    Initial commit

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+"use client"
+
+import ThreeScene from '../Components/ThreeScene';
+import { Canvas } from '@react-three/fiber';
+
+export default function Home() {
+  return (
+    <ThreeScene />
+  );
+}
