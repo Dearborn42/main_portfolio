3ed8425455d9ddc443836d922151ac7d767933b9
Dearborn42 <amurf26@outlook.com>
2024-04-16 15:11:26 -0700

    Working on text placement

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense, useEffect, useState, useRef, useLayoutEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Environment, MeshReflectorMaterial, Text } from '@react-three/drei'
+import { Environment, MeshReflectorMaterial, Text } from '@react-three/drei';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export default function App() {
+  gsap.registerPlugin(useGSAP);
+  const name = gsap.timeline();
+  const ref = useRef()
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const [posY, setPosY] = useState(0);
@@ -12,10 +17,17 @@ export default function App() {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
     console.log(window.innerWidth, window.innerHeight);
+  }, []);
+  useLayoutEffect(() => {
+    name.to(posY, {
+      duration:2.5,
+      ease: "bounce.out",
+      y: 0
+    })
   }, [])
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', margin: 0, padding: 0 }}>
-    <Canvas dpr={[1, 1.5]} shadows camera={{ position: [-15, 15, 18], fov: 35 }} gl={{ alpha: false }}>
+    <Canvas dpr={[1, 1.5]} shadows camera={{ position: [0, 7, 18], fov: 35 }} gl={{ alpha: false }}>
       <fog attach="fog" args={['#080808', 20, 40]} />
       <color attach="background" args={['#080808']} />
       <ambientLight intensity={1} />
@@ -26,7 +38,7 @@ export default function App() {
         {/* <ScrollControls pages={3}>
           <Train />
         </ScrollControls> */}
-        <Text fontSize={2} color="#ffffff" position={[0, posY, 4]}>
+        <Text name="name" fontSize={2} color="#ffffff" position={[0, posY, 4]}>
           Andrew Murphy
         </Text>
         <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
