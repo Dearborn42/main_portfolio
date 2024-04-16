572297e85e976cdbdaf673821c5ceeb5048af2f0
Dearborn42 <amurf26@outlook.com>
2024-04-15 23:32:42 -0700

    Update LOD.jsx

--- client/src/Components/LOD.jsx
+++ client/src/Components/LOD.jsx
@@ -1,39 +1,40 @@
-// import { useEffect } from 'react';
-// import { useThree } from '@react-three/fiber';
-// import { Detailed } from '@react-three/drei';
-// import { Frame, GithubPage } from './GithubPortal';
+import { useEffect } from 'react';
+import { useThree } from '@react-three/fiber';
+import { Detailed } from '@react-three/drei';
+import { Frame } from './GithubPortal';
+import Model from './Model';
 
-// export default function LOD({ setMeshArray, meshArray, active, setActive }) {
-//   const { scene } = useThree();
-//   useEffect(() => {
+export default function LOD({ setMeshArray, meshArray, active, setActive }) {
+  const { scene } = useThree();
+  useEffect(() => {
 
-//     const allMeshCoordinates = [];
+    const allMeshCoordinates = [];
 
-//     for (let j = 0; j < 20; j++) {
-//       const x = 8000 * (0.5 - Math.random());
-//       const y = 10000 * (0.5 - Math.random());
-//       const z = 8000 * (0.5 - Math.random());
-//       allMeshCoordinates.push({ x, y, z });
-//     }
+    for (let j = 0; j < 20; j++) {
+      const x = 8000 * (0.5 - Math.random());
+      const y = 10000 * (0.5 - Math.random());
+      const z = 8000 * (0.5 - Math.random());
+      allMeshCoordinates.push({ x, y, z });
+    }
 
-//     setMeshArray(allMeshCoordinates);
+    setMeshArray(allMeshCoordinates);
 
-//     return () => {
-//       scene.children.forEach(child => {
-//         if (child instanceof Detailed) {
-//           scene.remove(child);
-//         }
-//       });
-//     };
-//   }, []);
+    return () => {
+      scene.children.forEach(child => {
+        if (child instanceof Detailed) {
+          scene.remove(child);
+        }
+      });
+    };
+  }, [scene]);
 
-//   return (
-//     <group>
-//       {meshArray.map((x, i) => (
-//         <Frame key={i + `_key_id`} bg="#000000" name={`${i}_id`} active={active} setActive={setActive} meshArgs={[100, 16]} position={[x.x, x.y, x.z]}>
-//           <GithubPage />
-//         </Frame>
-//       ))}
-//     </group>
-//   )
-// }
+  return (
+    <group>
+      {meshArray.map((x, i) => (
+        <Frame key={i+"_key"} bg="#000000" name={i+"_name"} active={active} setActive={setActive} meshArgs={[100, 16]}>
+          <Model />
+        </Frame>
+      ))}
+    </group>
+  )
+}
