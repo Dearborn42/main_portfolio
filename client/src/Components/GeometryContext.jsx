5aa316645feab44ebcf8903bae0f77ecdbf50ee2
Dearborn42 <amurf26@outlook.com>
2024-04-18 13:32:26 -0700

    Some changes

--- client/src/Components/GeometryContext.jsx
+++ client/src/Components/GeometryContext.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useMemo, useContext } from "react";
+import { createContext, useMemo } from "react";
 import * as THREE from "three"
 
 export const GeometryContext = createContext();
--- client/src/Components/GithubPortal.jsx
+++ client/src/Components/GithubPortal.jsx
@@ -11,16 +11,14 @@ export function Frame({ bg, children, name, color, active, setActive, meshArgs, 
     easing.damp(portal.current, "blend", worldOpen ? 1 : 0, 0.2, delta)
   })
   return (
-    <group {...props}>
-        <mesh name={name} onDoubleClick={() => {
-            setActive(active === name ? null : name)
-        }}>
-          <sphereGeometry args={meshArgs} />
-          <MeshPortalMaterial ref={portal} blend={active === name ? 1 : 0}>
-            <color attach="background" args={[bg]} />
-            {children}
-          </MeshPortalMaterial>
-        </mesh>
-    </group>
+    <mesh {...props} name={name} onDoubleClick={() => {
+        setActive(active === name ? null : name)
+    }}>
+      <sphereGeometry args={meshArgs} />
+      <MeshPortalMaterial ref={portal} blend={active === name ? 1 : 0}>
+        <color attach="background" args={[bg]} />
+        {children}
+      </MeshPortalMaterial>
+    </mesh>
   )
 }
