08f2df9950d4173691a154089d32dc5541674b46
Dearborn42 <amurf26@outlook.com>
2024-04-22 13:11:14 -0700

    path button animation fixed

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -8,7 +8,6 @@ import { Flex } from "@react-three/flex";
 import MainPagesBackground from "@/Components/MainPagesBackground";
 import Scene from '@/Components/SceneFog';
 import Link from 'next/link';
-import { GoArrowLeft, GoArrowRight  } from "react-icons/go";
 
 export default function App() {
   const [dpr, setDpr] = useState(1.5);
@@ -28,21 +27,17 @@ export default function App() {
       <AdaptiveEvents />
       <Scene/>
       <Suspense fallback={null}>
-        <Flex justifyContent="center" flexWrap="wrap" alignItems="center" size={[1, 1, 2]}>
           <LandingText text={"Andrew Murphy"} placement={[0, 1.2, -5]}  size={.6}/>
-          <Flex justifyContent="center" flexWrap="wrap" alignItems="center" size={[1, .9, .1]}>
-            <LandingText
-              text={"“Any man's death diminishes me, because I am involved in mankind; and"} 
-              placement={[0, .6, -2]}
-              size={40}
-            />
-            <LandingText
-              text={"therefore never send to know for whom the bell tolls; it tolls for thee.”"} 
-              placement={[0, -.3, -1]}
-              size={40}
-            />
-          </Flex>
-        </Flex>
+          <LandingText
+            text={"“Any man's death diminishes me, because I am involved in mankind; and"} 
+            placement={[0, .6, -2]}
+            size={40}
+          />
+          <LandingText
+            text={"therefore never send to know for whom the bell tolls; it tolls for thee.”"} 
+            placement={[0, -.3, -1]}
+            size={40}
+          />
         <MainPagesBackground strength={1}/>
         <Environment preset="dawn" />
       </Suspense>
