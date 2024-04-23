5a0c5cb7bd8d3044754de4ae31ad87814c2aa948
Dearborn42 <amurf26@outlook.com>
2024-04-23 01:21:51 -0700

    resume page done

--- client/src/app/path/2/resume/page.jsx
+++ client/src/app/path/2/resume/page.jsx
@@ -1,149 +1,230 @@
-"use client";
-
-import { Canvas, useThree, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
-import { useEffect, Suspense } from "react";
-import fragmentShader from '@/glsl/fragmentShader.glsl';
-import vertextShader from '@/glsl/vertexShader.glsl';
-
-const SEPARATION = 100, AMOUNTX = 50, AMOUNTY = 50;
-
-let particles, count = 0;
-
-let mouseX = 0, mouseY = 0;
-
-
-
-function Render() {
-    const { gl, camera, scene } = useThree();
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    camera.lookAt( scene.position );
-
-    useFrame(()=>{
-        const positions = particles.geometry.attributes.position.array;
-        const scales = particles.geometry.attributes.scale.array;
-
-        let i = 0, j = 0;
-        for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
-            for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
-                positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
-                scales[ j ] = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 20 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 20;
-                i += 3;
-                j ++;
-            }
-        }
-
-        particles.geometry.attributes.position.needsUpdate = true;
-        particles.geometry.attributes.scale.needsUpdate = true;
-    });
-
-    gl.render( scene, camera );
-    return null;
-}
-
-function Scene() {
-    const { size, set, camera } = useThree();
-
-    useEffect(() => {
-        const newCamera = new THREE.PerspectiveCamera(75, size.width / size.height, 1, 10000);
-        set({ newCamera });
-        camera.position.y = 4;
-        camera.position.z = 70;
-    }, [size, set]);
-
-    return null;
-}
-
-function PointSetUp(){
-    const { scene } = useThree();
-    const numParticles = AMOUNTX * AMOUNTY;
-
-    const positions = new Float32Array( numParticles * 3 );
-    const scales = new Float32Array( numParticles );
-
-    let i = 0, j = 0;
-
-    for(let ix=0; ix<AMOUNTX; ix++){
-        for(let iy=0; iy<AMOUNTY; iy++){
-            positions[i] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ); // x
-            positions[i+1] = 0; // y
-            positions[i+2] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ); // z
-            scales[j] = 1;
-            i+=3;
-            j++;
-        }
-    }
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.setAttribute( 'scale', new THREE.BufferAttribute( scales, 1 ) );
-
-    const material = new THREE.ShaderMaterial( {
-        uniforms: {
-            color: { value: new THREE.Color( 0xffffff ) },
-        },
-        vertexShader: vertextShader,
-        fragmentShader: fragmentShader
-    });
-
-    particles = new THREE.Points( geometry, material );
-    scene.add( particles );
-
-    return null;
-}
-
-
-
-function Animate() {
-    const { gl, camera, scene } = useThree();
-
-    camera.position.y = 30;
-    camera.position.z = 70;
-
-    useEffect(() => {
-        const positions = particles.geometry.attributes.position.array;
-        const scales = particles.geometry.attributes.scale.array;
-
-        const updateParticles = () => {
-            count += 0.1;
-
-            let i = 0, j = 0;
-            for (let ix = 0; ix < AMOUNTX; ix++) {
-                for (let iy = 0; iy < AMOUNTY; iy++) {
-                    positions[i + 1] = (Math.sin((ix + count) * 0.3) * 50) + (Math.sin((iy + count) * 0.5) * 50);
-                    scales[j] = (Math.sin((ix + count) * 0.3) + 1) * 20 + (Math.sin((iy + count) * 0.5) + 1) * 20;
-                    i += 3;
-                    j++;
-                }
-            }
-
-            particles.geometry.attributes.position.needsUpdate = true;
-            particles.geometry.attributes.scale.needsUpdate = true;
-
-        };
-
-        const animate = () => {
-            updateParticles();
-            requestAnimationFrame(animate);
-        };
-
-        animate();
-
-        return () => cancelAnimationFrame(animate);
-    }, [gl, camera, scene, particles, mouseX, mouseY]);
-    return null;
-}
+"use client"
+import Link from "next/link"
+import { FaArrowLeft } from "react-icons/fa6"
 
 export default function Resume(){
     return (
-        <div className='fixed top-0 bottom-0 left-0 right-0'>
-            <Canvas>
-                <Scene />
-                <Suspense fallback={false}>
-                    <PointSetUp />
-                </Suspense>
-                <Animate />
-            </Canvas>
-        </div>
+        <main class="main-content">
+            <div className="fixed top-0 right-0 z-10 flex justify-between">
+                <Link href="/path/2" className="fixed top-12 text-sm sm:text-4xl left-20">
+                    <FaArrowLeft />
+                </Link>
+            </div>
+        <section class="left-section">
+            <div class="left-content">
+                <div class="profile">
+                    <div class="image">
+                    </div>
+                    <h2 class="name">Andrew Murphy</h2>
+                    <p class="career">Backend Developer</p>
+                </div>
+                <div class="skills-section">
+                    <h3 class="main-title">Languages</h3>
+                    <ul>
+                        <li>
+                            <p class="skill-title">Node.js</p>
+                            <div class="progress-bar">
+                                <div class="progress js-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">Python</p>
+                            <div class="progress-bar">
+                                <div class="progress ps-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">Java</p>
+                            <div class="progress-bar">
+                                <div class="progress j-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">React</p>
+                            <div class="progress-bar">
+                                <div class="progress c-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">Next JS</p>
+                            <div class="progress-bar">
+                                <div class="progress ps-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">MongoDb</p>
+                            <div class="progress-bar">
+                                <div class="progress n-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">CSS</p>
+                            <div class="progress-bar">
+                                <div class="progress ps-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">Mongoose</p>
+                            <div class="progress-bar">
+                                <div class="progress ps-progress"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <p class="skill-title">HTML</p>
+                            <div class="progress-bar">
+                                <div class="progress ps-progress"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="right-section">
+            <div class="right-main-content">
+                <section class="about sect">
+                    <h2 class="right-title">About Me</h2>
+                    <p class="para">
+                        I am a high school honor student with a 4.0 GPA, who is on
+                        target to earn an Honor High School diploma in May of
+                        2024. In addition, I attend West-MEC vocational school
+                        studying computer coding. As a student at West-MEC, I am
+                        also a member of Future Business Leaders of American
+                        (FBLA) . My involvement in West-MEC and FBLA have
+                        provided me with the opportunity to work along side fellow
+                        students with varying backgrounds, opinions, and work
+                        styles. At West-MEC, the majority of our work is focused on
+                        working in collaborative groups, with assigned roles in said
+                        groups. These groups have taught me to lead and direct
+                        others in a quick, efficient, and effective manner.
+                        <br />
+                    </p>
+                </section>
+
+                <section class="experince sect">
+                    <h2 class="right-title">Experience</h2>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">WBLA</h5>
+                            <p class="para">10 weeks</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Team lead/backend developer</h5>
+                                <p class="para">
+                                    Lead a team in the create of a portfolio for a client that partnered with WBLA.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">West-Mec</h5>
+                            <p class="para">10 weeks</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Team lead/backend developer</h5>
+                                <p class="para">
+                                    Lead a team of 2 in developing a software system for furture students of West-Mec Medical programs.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">Profestion</h5>
+                            <p class="para">2019 - 2020</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Position</h5>
+                                <p class="para">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cupiditate vitae 
+                                    voluptatem deleniti, 
+                                    laboriosam odio nobis quae fugit facilis quo.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">Five Guys</h5>
+                            <p class="para">2023-present</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Crew Member</h5>
+                                <p class="para">
+                                    Complete every nessecary task required such as cooking, cleaning, handling transactions, etc.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="education sect">
+                    <h2 class="right-title">education</h2>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">Thunderbird High School</h5>
+                            <p class="para">2020 - 2024</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Thunderbird High School</h5>
+                                <p class="para">
+                                    Graduated with a GPA of 3.9 and completed many college level courses during my time there.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">West-Mec</h5>
+                            <p class="para">2022 - 2024</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">West-Mec</h5>
+                                <p class="para">
+                                    Trade school were I was taught many skills I have today, and also completed many college level classes.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">Arizona State University</h5>
+                            <p class="para">2024-2028</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Bachelors in Comp Sci: Cyber Security</h5>
+                                <p class="para">
+                                    Working on it right now.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="awards sect">
+                    <h2 class="right-title">Awards</h2>
+                    <div class="timeline">
+                        <div class="left-tl-content">
+                            <h5 class="tl-title">FBLA National Competetor</h5>
+                            <p class="para">July 2023</p>
+                        </div>
+                        <div class="right-tl-content">
+                            <div class="tl-content">
+                                <h5 class="tl-title-2">Event</h5>
+                                <p class="para">
+                                    Placed top 10 in state in a cybersecurity competition.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </main>
     )
 }
