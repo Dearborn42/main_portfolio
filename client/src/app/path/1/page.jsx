b68acdbe619136b9fef6751c5c3ecdf9969cd2ab
Dearborn42 <amurf26@outlook.com>
2024-04-25 01:35:40 -0700

    Update page.jsx

--- client/src/app/path/1/page.jsx
+++ client/src/app/path/1/page.jsx
@@ -14,10 +14,10 @@ import { FaArrowLeft } from 'react-icons/fa6'
 const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970), link: "https://github.com/Dearborn42/FBLA-App" },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: "https://res.cloudinary.com/dkilzmi3w/image/upload/v1714033388/Screenshot_2024-04-25_012251_pwhhft.png", link: "https://github.com/Dearborn42/FBLA-App" },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430), link: "https://github.com/Dearborn42/mern-blueprint" },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452), link: "https://scurtisphd.com/home" },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: "https://res.cloudinary.com/dkilzmi3w/image/upload/v1714034088/Screenshot_2024-04-25_013359_gr4t2s.png", link: "https://github.com/Dearborn42/mern-blueprint" },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: "https://res.cloudinary.com/dkilzmi3w/image/upload/v1714033733/Screenshot_2024-03-24_230537_fdpwxz.png", link: "https://scurtisphd.com/home" },
   // Left
   { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482), link: "https://github.com/Dearborn42/React-TopDown-Platormer" },
   { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185), link: "https://github.com/Dearborn42/TaskManager" },
